123b17b0e81f6b5f966b204a7003759972730928
lokyshka <golovanov.roma72@bk.ru>
2026-08-26 14:59:54 +0300

    в файл сборки добавлено создание образа dmg.

--- build.sh
+++ build.sh
@@ -27,6 +27,55 @@ cat > builds/lokyvault.app/Contents/Info.plist <<EOF
 EOF
 echo "macOS app done!"
 
+# macOS app packing
+backg="/Users/lokyshka/.cache/lvault-backg.png"
+rm -f "builds/temp.dmg" "builds/lokyvault.dmg"
+
+hdiutil create -quiet -size 50m -fs HFS+ -volname "lokyvault" "builds/temp.dmg"
+hdiutil attach -quiet "builds/temp.dmg"
+
+cp -R builds/lokyvault.app "/Volumes/lokyvault"
+ln -s /Applications "/Volumes/lokyvault/Applications"
+mkdir "/Volumes/lokyvault/.background"
+cp "$backg" "/Volumes/lokyvault/.background/background.png"
+
+sleep 1
+osascript <<EOF
+tell application "Finder"
+    set bgfile to (POSIX file "/Volumes/lokyvault/.background/background.png") as alias
+    
+    tell disk "lokyvault"
+        open
+        set current view of container window to icon view
+        set toolbar visible of container window to false
+        set statusbar visible of container window to false
+        
+        set the bounds of container window to {400, 100, 1060, 500}
+        
+        set opts to the icon view options of container window
+        set arrangement of opts to not arranged
+        set icon size of opts to 128
+        set background picture of opts to bgfile
+        
+        set position of item "lokyvault.app" of container window to {160, 200}
+        set position of item "Applications" of container window to {500, 200}
+        
+        close
+        open
+        update without registering applications
+        delay 2
+    end tell
+end tell
+EOF
+SetFile -a C "/Volumes/lokyvault"
+
+sync
+hdiutil detach -quiet "/Volumes/lokyvault" -force
+
+hdiutil convert -quiet "builds/temp.dmg" -format ULFO -o "builds/lokyvault.dmg"
+rm "builds/temp.dmg"
+echo "macOS app packing done!"
+
 # windows
 export CGO_ENABLED=1
 export CC="/Users/lokyshka/.compilers/zig-wrapper.sh"
